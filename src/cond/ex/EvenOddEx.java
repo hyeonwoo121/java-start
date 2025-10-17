f097574d88025603260c34040cd5347002073050
이현우 <justice3350@naver.com>
2025-10-18 01:11:44 +0900

    코딩테스트 문제1018

--- src/cond/ex/EvenOddEx.java
+++ src/cond/ex/EvenOddEx.java
@@ -3,8 +3,8 @@ package cond.ex;
 public class EvenOddEx {
 
     public static void main(String[] args) {
-        int x = 3;
-        String result = (x % 2 == 0) ? "짝수" : "홀수";
-        System.out.println("x = " + x + ", " + result);
+        int x = 5;
+        String result = (x % 2 == 0) ? "짝수":"홀수";
+        System.out.println("x = " + x + "," + result);
     }
 }
---
+++ src/problem/Question10.java
@@ -0,0 +1,16 @@
+package problem;
+
+public class Question10 {
+    public static void main(String[] args) {
+
+        double rating = 7.3;
+
+        if(rating <= 9){
+            System.out.println("어바웃타임을 추천합니다");
+        }if(rating <= 8){
+            System.out.println("토이스토리를 추천합니다");
+        }if(rating <= 7){
+            System.out.println("고질라를 추천합니다");
+        }
+    }
+}
